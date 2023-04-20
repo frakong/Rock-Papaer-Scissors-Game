5e617b2bcbd71e118c289dea6fc268334d2882a7
Franklin Kong <frakong@ucdavis.edu>
2023-04-20 11:08:27 -0700

    Added result display

--- index.js
+++ index.js
@@ -6,20 +6,27 @@ const rockButton = document.querySelector(".rock-button");
 const paperButton = document.querySelector(".paper-button");
 const scissorsButton = document.querySelector(".scissors-button");
 
+//1 = Rock, 2 = Paper, 3 = Scissors
+let playerChoiceNumber = 0;
+let computerChoiceNumber = 0;
+
 rockButton.addEventListener("click", () => {
   playerLine.innerHTML = "Player Selects: Rock";
+  playerChoiceNumber = 1;
   computerPlays();
-  //updateResult();
+  updateResult();
 })
 paperButton.addEventListener("click", () => {
   playerLine.innerHTML = "Player Selects: Paper";
+  playerChoiceNumber = 2;
   computerPlays();
-  //updateResult();
+  updateResult();
 })
 scissorsButton.addEventListener("click", () => {
   playerLine.innerHTML = "Player Selects: Scissors";
+  playerChoiceNumber = 3;
   computerPlays();
-  //updateResult();
+  updateResult();
 })
 
 function computerPlays() {
@@ -27,16 +34,29 @@ function computerPlays() {
   switch (computerItem){
     case 1:
       computerLine.innerHTML = "Computer Selects: Rock";
+      computerChoiceNumber = 1;
       break;
     case 2:
       computerLine.innerHTML = "Computer Selects: Paper";
+      computerChoiceNumber = 2;
       break;
     case 3:
       computerLine.innerHTML = "Computer Selects: Scissors";
+      computerChoiceNumber = 3;
       break;
   }
 }
 
-
+function updateResult() {
+  if (playerChoiceNumber == computerChoiceNumber){
+    resultLine.innerHTML = "DRAW";
+  }
+  else if (playerChoiceNumber == (computerChoiceNumber % 3) + 1){
+    resultLine.innerHTML = "YOU WIN";
+  }
+  else {
+    resultLine.innerHTML = "YOU LOSE";
+  }
+}
 
 
