c5cd17919c61df96bbfb7e886a522a4e70b79f05
Franklin Kong <frakong@ucdavis.edu>
2023-04-20 10:29:21 -0700

    Added function for computer playing

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const scissorsButton = document.querySelector(".scissors-button");
 
 rockButton.addEventListener("click", () => {
   playerLine.innerHTML = "Player Selects: Rock";
+  computerPlays();
+  //updateResult();
 })
 paperButton.addEventListener("click", () => {
   playerLine.innerHTML = "Player Selects: Paper";
+  computerPlays();
+  //updateResult();
 })
 scissorsButton.addEventListener("click", () => {
   playerLine.innerHTML = "Player Selects: Scissors";
+  computerPlays();
+  //updateResult();
 })
+
+function computerPlays() {
+  let computerItem = Math.floor(Math.random()*3) + 1; //Returns integer that's 1, 2, or 3
+  switch (computerItem){
+    case 1:
+      computerLine.innerHTML = "Computer Selects: Rock";
+      break;
+    case 2:
+      computerLine.innerHTML = "Computer Selects: Paper";
+      break;
+    case 3:
+      computerLine.innerHTML = "Computer Selects: Scissors";
+      break;
+  }
+}
+
+
